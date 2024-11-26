a5782eaafeb73a2b064431b1b1677773ebf56e96
Matthew Turner <matthewfturner@gmail.com>
2024-11-26 22:47:58 +0000

    My personal website

---
+++ precache-manifest.53d57d328f9a18184af7cb4108c138c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad754f1751c9c6a77910",
+    "url": "/static/js/main.ad754f17.chunk.js"
+  },
+  {
+    "revision": "211c50ce42c6177e673a",
+    "url": "/static/js/1.211c50ce.chunk.js"
+  },
+  {
+    "revision": "ad754f1751c9c6a77910",
+    "url": "/static/css/main.77a388f0.chunk.css"
+  },
+  {
+    "revision": "211c50ce42c6177e673a",
+    "url": "/static/css/1.c00a6af3.chunk.css"
+  },
+  {
+    "revision": "8541d1c2c28327278b6f95241cc2523e",
+    "url": "/index.html"
+  }
+];
